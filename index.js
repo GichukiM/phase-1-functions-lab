0d920cad13f2348a74b0b6dd44fd4c03f7023576
Collins Gichuki <gichukimuchiri80@gmail.com>
2024-03-21 06:09:41 +0300

    complete functions-lab

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+  const scuberHq = 42;
+
+  if (pickupLocation >= scuberHq) {
+    return pickupLocation - scuberHq;
+  } else {
+    return scuberHq - pickupLocation;
+  }
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+  const distanceInBlocks = distanceFromHqInBlocks(pickupLocation);
+  const distanceInFeetPerBlock = 264;
+
+  return distanceInBlocks * distanceInFeetPerBlock;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  const distanceTravelledInBlocks = destination - start;
+  const destinationBelowDistance = start - destination;
+  const distanceInFeetPerBlock = 264;
+
+  if (destination >= start) {
+    return distanceTravelledInBlocks * distanceInFeetPerBlock;
+  } else {
+    return destinationBelowDistance * distanceInFeetPerBlock
+  }
+}
+
+
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+  
+
+  if (distance <= 400) {
+    return 0;
+  } else if (distance >= 400 && distance <= 2000) {
+    const distanceAfterFreeSample = distance - 400;
+    const fare = distanceAfterFreeSample * 0.02;
+
+    return fare
+  } else if (distance > 2000 && distance <= 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
